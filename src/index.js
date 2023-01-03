8a9e04d55dd51df8f22fe77c62c47e32def5e1b5
gabicilfone <cilfonegabriel@gmail.com>
2023-01-03 16:27:54 -0300

    fix linters error

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import './style.css';
 import displayScores from './displayScore';
 import addScore from './addScore';
 
-let scores=[];
+const scores = [];
 
 const sectionForScores = document.querySelector('#score-container');
 displayScores(scores, sectionForScores);
 
 class Score {
-    constructor(name, score){
-        this.name = name;
-        this.score = score;
-    }
-};
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+}
 
 const myForm = document.getElementById('my-form');
 myForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let name = document.getElementById('name').value;
-    let score = document.getElementById('score').value;
-    let newscore = new Score(name, score);
-    addScore(scores, newscore);
-    myForm.reset();
-    displayScores(scores, sectionForScores);
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  const newscore = new Score(name, score);
+  addScore(scores, newscore);
+  myForm.reset();
+  displayScores(scores, sectionForScores);
 });
