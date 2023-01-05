82259785028ab7b227872c71691036481f81c621
gabicilfone <cilfonegabriel@gmail.com>
2023-01-05 13:44:26 -0300

    fix linters error

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import getScores from './getScores';
 import addScore from './addScore.js';
 import sortScores from './sortScore.js';
 
-
 const sectionForScores = document.querySelector('#score-container');
 
 const displayScores = async () => {
   const data = await getScores();
   let scoreGenerator = '';
   sortScores(data.result);
-  data.result.forEach(user => {
+  data.result.forEach((user) => {
     scoreGenerator += `<li><span class="bold">Name:</span> ${user.user}  <span class="bold">  Score:</span> ${user.score}</li>`;
   });
   sectionForScores.innerHTML = scoreGenerator;
